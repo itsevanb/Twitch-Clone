2fdb1297c73d58065a9e363ef9dadc5b288d65f8
Evan <116227653+itsevanb@users.noreply.github.com>
2023-12-15 11:53:15 -0600

    begin styling
    using layout folder in our auth

--- video-streaming/app/page.tsx
+++ video-streaming/app/page.tsx
@@ -1,8 +1,10 @@
-
+import { UserButton } from "@clerk/nextjs"
 export default function Page() {
   return (
-    <p>
-      Only authenticated users can see this page.
-    </p>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton 
+      afterSignOutUrl="/"/>
+    </div>
   )
 }
