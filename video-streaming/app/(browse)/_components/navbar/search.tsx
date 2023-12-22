5b1f28fd561bb354406a5f51a23361f3c90fba5a
Evan <116227653+itsevanb@users.noreply.github.com>
2023-12-22 15:00:18 -0600

    Update search.tsx
    importing all needed to build search functionality

--- video-streaming/app/(browse)/_components/navbar/search.tsx
+++ video-streaming/app/(browse)/_components/navbar/search.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import qs from "query-string";
+import { useState } from "react";
+import { SearchIcon, X } from "lucide-react";
+import { useRouter } from "next/router";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export const Search = () => {
     return (
         <div>
