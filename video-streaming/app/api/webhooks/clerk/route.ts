4cf7e3f612ff1908851448220df44d831f0532f0
Evan <116227653+itsevanb@users.noreply.github.com>
2023-12-22 13:54:06 -0600

    Update route.ts
    user actions now synchronized with database

--- video-streaming/app/api/webhooks/clerk/route.ts
+++ video-streaming/app/api/webhooks/clerk/route.ts
@@ -58,5 +58,37 @@ export async function POST(req: Request) {
   });
 }
 
+  if (eventType === 'user.updated') {
+    const currentUser = await db.user.findUnique({
+        where: {
+            externalUserId: payload.data.id,
+        }
+    })
+
+    if (!currentUser) {
+        return new Response('User not found', {
+            status: 404
+        })
+    }
+
+    await db.user.update({
+        where: {
+            externalUserId: payload.data.id,
+        },
+        data: {
+            username: payload.data.username,
+            imageUrl: payload.data.image_url,
+        },
+    })
+}
+
+    if (eventType === 'user.deleted') {
+        await db.user.delete({
+            where: {
+                externalUserId: payload.data.id,
+            }
+        })
+    }
+
   return new Response('', { status: 200})
 }
