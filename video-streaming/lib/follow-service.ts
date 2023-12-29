a0755f2bd9b9314a0df9b52054529c9e160a0b48
Evan <116227653+itsevanb@users.noreply.github.com>
2023-12-28 20:55:24 -0600

    adding to lib modules
    building following service

---
+++ video-streaming/lib/follow-service.ts
@@ -0,0 +1,32 @@
+import { db } from '@/lib/db';
+import { getSelf } from '@/lib/auth-service';
+
+export const isFollowingUser = async (id: string) => {
+    try {
+        const self = await getSelf();
+
+        const otherUser = await db.user.findUnique({
+            where: { id },
+        });
+
+        if (!otherUser) {
+            throw new Error('User not found');
+        }
+
+        if (otherUser.id === self.id) {
+            return true;
+        }
+
+        const existingFollow = await db.follow.findFirst({
+            where: {
+                followerId: self.id,
+                followingId: otherUser.id,
+            },
+        });
+
+        return !!existingFollow;
+        
+    } catch {
+        return false;
+    }
+};
