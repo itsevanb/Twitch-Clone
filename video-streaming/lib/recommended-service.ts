217cbaddcfcb90e3ed60cd9f672b41f4d2927cc0
Evan <116227653+itsevanb@users.noreply.github.com>
2023-12-30 14:41:03 -0600

    Update recommended-service.ts
    just spacing

--- video-streaming/lib/recommended-service.ts
+++ video-streaming/lib/recommended-service.ts
@@ -2,50 +2,50 @@ import { db } from "@/lib/db";
 import { getSelf } from "./auth-service";
 
 export const getRecommended = async () => {
-    let userId;
+  let userId;
 
-    try {
-        const self = await getSelf();
-        userId = self.id;
-    } catch {
-        userId = null;
-    }
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch {
+    userId = null;
+  }
 
-    let users = []
+  let users = [];
 
-    if (userId) {
-        users = await db.user.findMany({
-            // Find all users that are not the current user and not followed by the current user
-            // This is the same as finding all users that are not followed by the current user
-            where: {
-                AND: [
-                    {
-                        NOT: {
-                            id: userId,
-                    },
-                },
-                {
-                    NOT: {
-                        followedBy: {
-                            some: {
-                                followerId: userId,
-                            },
-                        },
-                    },
-                },
-            ],
-        },
-            orderBy: {
-                createdAt: "desc",
+  if (userId) {
+    users = await db.user.findMany({
+      // Find all users that are not the current user and not followed by the current user
+      // This is the same as finding all users that are not followed by the current user
+      where: {
+        AND: [
+          {
+            NOT: {
+              id: userId,
             },
-        });
-    } else {
-        users = await db.user.findMany({
-            orderBy: {
-                createdAt: "desc",
+          },
+          {
+            NOT: {
+              followedBy: {
+                some: {
+                  followerId: userId,
+                },
+              },
             },
-        });
-    }
+          },
+        ],
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
 
-    return users;
-}
+  return users;
+};
