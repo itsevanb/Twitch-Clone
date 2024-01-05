698de360799c92827ff9f94310b15c34f5df2119
Evan <116227653+itsevanb@users.noreply.github.com>
2024-01-05 11:08:54 -0600

    chat settings updated
    connected to database and functional

--- video-streaming/app/(dashboard)/u/[username]/chat/page.tsx
+++ video-streaming/app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,5 +1,6 @@
 import { getSelf } from "@/lib/auth-service";
 import { getStreamByUserId } from "@/lib/stream-service";
+import { ToggleCard } from "./_components/toggle-card";
 
 export const ChatPage = async () => {
     const self = await getSelf();
@@ -9,13 +10,30 @@ export const ChatPage = async () => {
         throw new Error("Stream not found");
     }
 
-    
+
     return (
         <div className="p-6">
             <div className="mb-4">
                 <h1 className="text-2xl font-bold">
                     Chat Settings
                 </h1>
+            </div>
+            <div className="space-y-4">
+                <ToggleCard
+                field="isChatEnabled"
+                label="Enable Chat"
+                value={stream.isChatEnabled}
+                />
+                <ToggleCard
+                field="isChatDelayed"
+                label="Enable Chat Delay"
+                value={stream.isChatDelayed}
+                />
+                <ToggleCard
+                field="isChatFollowersOnly"
+                label="Enable Followers Only Chat"
+                value={stream.isChatFollowersOnly}
+                />
             </div>
         </div>
     );
