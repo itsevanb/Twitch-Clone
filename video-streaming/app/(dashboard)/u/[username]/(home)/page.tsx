3aa323d23daaac010fec6c3f1136b154c3936087
Evan <116227653+itsevanb@users.noreply.github.com>
2024-01-04 18:49:34 -0600

    creator dashboard created
    navbar
    sidebar

---
+++ video-streaming/app/(dashboard)/u/[username]/(home)/page.tsx
@@ -0,0 +1,9 @@
+const CreatorPage = () => {
+    return (
+        <div>
+            Creator Page
+        </div>
+    );
+};
+
+export default CreatorPage;
---
+++ video-streaming/app/(dashboard)/u/[username]/layout.tsx
@@ -0,0 +1,37 @@
+import { getSelfByUsername } from "@/lib/auth-service";
+
+import { redirect } from "next/navigation";
+
+import { Navbar } from "./_components/navbar";
+import { Sidebar } from "./_components/sidebar";
+import { Container } from "./_components/container";
+
+interface CreatorLayoutProps {
+    params: { username: string };
+    children: React.ReactNode;
+};
+
+const CreatorLayout = async ({
+    params,
+    children,
+}: CreatorLayoutProps) => {
+    const self = await getSelfByUsername(params.username);
+
+    if (!self) {
+        redirect("/")
+    }
+
+    return (
+        <>
+            <Navbar />
+            <div className="flex h-full pt-20">
+                <Sidebar />
+                <Container>
+                    {children}
+                </Container>
+            </div>
+        </>
+    )
+};
+
+export default CreatorLayout;
