cada33892a042076319d38ed0400f1426a252b63
Evan <116227653+itsevanb@users.noreply.github.com>
2024-01-17 13:39:30 -0600

    updated blocking ability
    can block guest users now

--- video-streaming/actions/block.ts
+++ video-streaming/actions/block.ts
@@ -1,16 +1,36 @@
 "use server";
 
-import { blockUser, unblockUser } from "@/lib/block-service";
 import { revalidatePath } from "next/cache";
+import { getSelf } from "@/lib/auth-service";
+import { blockUser, unblockUser } from "@/lib/block-service";
+
+import { RoomServiceClient } from "livekit-server-sdk";
+
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!,
+);
 
 export const onBlock = async (id: string) => {
-    const blockedUser = await blockUser(id);
+    const self = await getSelf();
 
-    revalidatePath("/");
+    let blockedUser;
 
-    if (blockedUser) {
-        revalidatePath(`/${blockedUser.blocked.username}`);
+    try {
+        blockedUser = await blockUser(id);
+    } catch {
+        // this means user is a guest 
+    } 
+
+    try {
+        // remove guest from room using room service
+        await roomService.removeParticipant(self.id, id)
+    } catch {
+        // this means user is not in the room
     }
+
+    revalidatePath(`/u/${self.username}/community`);
 
     return blockedUser;
 };
--- video-streaming/app/(browse)/[username]/page.tsx
+++ video-streaming/app/(browse)/[username]/page.tsx
@@ -1,37 +1,39 @@
-import { isFollowingUser } from "@/lib/follow-service";
-import { getUserByUsername } from "@/lib/user-service";
-import { isBlockedByUser } from "@/lib/block-service";
-
 import { notFound } from "next/navigation";
-import { Actions } from "./_components/actions";
+
+import { getUserByUsername } from "@/lib/user-service";
+import { isFollowingUser } from "@/lib/follow-service";
+import { isBlockedByUser } from "@/lib/block-service";
+import { StreamPlayer } from "@/components/stream-player";
 
 interface UserPageProps {
-    params: {
-        username: string;
-    };
+  params: {
+    username: string;
+  };
 };
 
-const UserPage = async ({ 
-    params
+const UserPage = async ({
+  params
 }: UserPageProps) => {
-    const user = await getUserByUsername(params.username);
+  const user = await getUserByUsername(params.username);
 
-    if (!user) {
-        notFound();
-    }
+  if (!user || !user.stream) {
+    notFound();
+  }
 
-    const isFollowing = await isFollowingUser(user.id);
-    const isBlocked = await isBlockedByUser(user.id);
+  const isFollowing = await isFollowingUser(user.id);
+  const isBlocked = await isBlockedByUser(user.id);
 
-    return (
-        <div className="flex flex-col gap-y-4">
-            <p>User: {user.username}</p>
-            <p>user ID: {user.id} </p>
-            <p>is following: {`${isFollowing}`} </p>
-            <p>is blocked by this user: {`${isBlocked}`}</p>
-            <Actions userId={user.id} isFollowing={isFollowing} />
-        </div>
-    );
-};
+  if (isBlocked) {
+    notFound();
+  }
 
+  return ( 
+    <StreamPlayer
+      user={user}
+      stream={user.stream}
+      isFollowing={isFollowing}
+    />
+  );
+}
+ 
 export default UserPage;
