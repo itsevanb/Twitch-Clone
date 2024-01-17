d7da4e6ef4f2a4b09af74b24fcbf8f00afab006b
Evan <116227653+itsevanb@users.noreply.github.com>
2024-01-17 14:52:31 -0600

    Update thumbnail.tsx
    comments

--- video-streaming/components/thumbnail.tsx
+++ video-streaming/components/thumbnail.tsx
@@ -19,6 +19,7 @@ export const Thumbnail = ({
  }: ThumbnailProps) => {
   let content;
 
+  // If there is no thumbnail, we'll show the user's avatar instead.
   if (!src) {
     content = (
       <div className="bg-background flex flex-col items-center justify-center gap-y-4 h-full w-full transition-transform group-hover:translate-x-2 group-hover:-translate-y-2 rounded-md">
@@ -31,6 +32,7 @@ export const Thumbnail = ({
         />
       </div>
     )
+    // If there is a thumbnail, we'll show it.
   } else {
     content = (
       <Image
@@ -42,6 +44,7 @@ export const Thumbnail = ({
     )
   }
 
+  // If the stream is live, we'll show the live badge.
   return (
     <div className="group aspect-video relative rounded-md cursor-pointer">
       <div className="rounded-md absolute inset-0 bg-[#2d4b4f] opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center" />
@@ -55,6 +58,7 @@ export const Thumbnail = ({
   );
 };
 
+// This is the skeleton that will be shown while the thumbnail is loading.
 export const ThumbnailSkeleton = () => {
   return (
     <div className="group aspect-video relative rounded-xl cursor-pointer">
