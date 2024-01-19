9a6a55fefa65c8aa5c7200692ddaf6a88bab0b3e
Evan <116227653+itsevanb@users.noreply.github.com>
2024-01-18 18:47:11 -0600

    Update page.tsx

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -5,28 +5,28 @@ import { UrlCard } from "./_components/url-card";
 import { KeyCard } from "./_components/key-card";
 import { ConnectModal } from "./_components/connect-modal";
 
-export const KeysPage = async () => {
-    const self = await getSelf();
-    const stream = await getStreamByUserId(self.id);
+const KeysPage = async () => {
+  const self = await getSelf();
+  const stream = await getStreamByUserId(self.id);
 
-    if (!stream) {
-        throw new Error("Stream not found")
-    }
+  if (!stream) {
+    throw new Error("Stream not found");
+  }
 
-    return (
-        <div className="p-6">
-            <div className="flex items-center justify-between mb-4">
-                <h1 className="text-2xl font-bold">
-                    Keys & URLs
-                </h1>
-                <ConnectModal />
-            </div>
-            <div className="space-y-4">
-                <UrlCard value={stream.serverUrl} />
-                <KeyCard value={stream.streamKey} />
-            </div>
-        </div>
-    );
+  return ( 
+    <div className="p-6">
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-2xl font-bold">
+          Keys & URLs
+        </h1>
+        <ConnectModal />
+      </div>
+      <div className="space-y-4">
+        <UrlCard value={stream.serverUrl} />
+        <KeyCard value={stream.streamKey} />
+      </div>
+    </div>
+  );
 };
-
+ 
 export default KeysPage;
